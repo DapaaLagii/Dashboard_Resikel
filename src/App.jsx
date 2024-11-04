7c545dbed383ccf5c8aaa222a6a99dd1f3430597
DapaaLagii <nandriadaffa02@gmail.com>
2024-11-04 18:14:52 +0700

    tes beberapa code

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
 // import { useState } from 'react'
-// import { React } from 'react'
+// import Footer from "/components/Footer";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-// import './index.css'
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import './index.css';
+import { BrowserRouter as Routes, Route, Router } from "react-router-dom";
 import Beranda from "./pages/Beranda";
 import Tentang from "./pages/Tentang";
 import Layanan from "./pages/Layanan";
 import Program from "./pages/Program";
 import Navbar from "./components/Navbar";
-//import Footer from "./components/Footer";
+
+
 
 
 function App() {
     return (
-      <>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Beranda />} />
-          <Route path="/tentang" element={<Tentang />} />
-          <Route path="/layanan" element={<Layanan />} />
-          <Route path="/program" element={<Program />} />
-        </Routes>
-        
-
-      
-      </>
+      <Router>       
+          <Navbar/>
+            <Routes>
+              <Route path="/" element={<Beranda />} />
+              <Route path="/tentang" element={<Tentang />} />
+              <Route path="/layanan" element={<Layanan />} />
+              <Route path="/program" element={<Program />} />
+            </Routes>    
+      </Router>
     );
   }
   
--- src/pages/Beranda.jsx
+++ src/pages/Beranda.jsx
@@ -3,7 +3,46 @@ import React from "react";
 const Beranda =  () => {
     return (
         <div>
-            
+            <div className="container">
+      <div className="header">
+        <div className="social">
+          <a href="#" className="icon">
+            <i className="icon-ig"></i>
+            @Resikel
+          </a>
+          <a href="#" className="icon">
+            <i className="icon-gmail"></i>
+            resikelbussines@gmail.com
+          </a>
+          <a href="#" className="icon">
+            <i className="icon-phone"></i>
+            123456789012
+          </a>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#">Beranda</a></li>
+            <li><a href="#">Tentang Kami</a></li>
+            <li><a href="#">Layanan</a></li>
+            <li><a href="#">Program</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div className="main">
+        <div className="content">
+          <h1>Malas Buang Sampah?</h1>
+          <h2>RESIKEL Aja</h2>
+          <div className="buttons">
+            <button>Unduh Aplikasi</button>
+            <button>Lihat Selengkapnya</button>
+          </div>
+        </div>
+        <div className="image">
+          <img src="/images/image.png" alt="image" />
+          <img src="./Image/nokia.png" alt="phone" />
+        </div>
+      </div>
+    </div>
         </div>
     );
 }
