ab3e61e8110ce5ea728d4e45c62c3a2d8ddc3f36
DapaaLagii <nandriadaffa02@gmail.com>
2024-11-08 18:40:00 +0700

    add keyframe and animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,17 @@ module.exports={
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'slide-down': {
+          '0%': { transform: 'translateY(-100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        }
+    },
+    animation: {
+      'slide-down': 'slide-down 0.3s ease-out',
+    }
   },
-  plugins: [],
+    plugins: [],
+  }
 }
-
