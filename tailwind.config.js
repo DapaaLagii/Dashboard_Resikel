c82497db7d03ad9e442f59e870551a397e1b87d8
DapaaLagii <nandriadaffa02@gmail.com>
2024-12-19 22:28:42 +0700

    redeploy vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports={
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}"
@@ -14,17 +14,16 @@ module.exports={
         'xl': '1280px',
         '2xl': '1536px',
       },
-
       keyframes: {
         'slide-down': {
           '0%': { transform: 'translateY(-100%)', opacity: '0' },
           '100%': { transform: 'translateY(0)', opacity: '1' },
         }
+      },
+      animation: {
+        'slide-down': 'slide-down 0.3s ease-out',
+      }
     },
-    animation: {
-      'slide-down': 'slide-down 0.3s ease-out',
-    }
   },
-    plugins: [],
-  }
+  plugins: [],
 }
